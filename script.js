0f9e233672b7d2256db934c024c6115758fadc8b
GabrielF <gabrieldossantos42r@gmail.com>
2023-06-20 21:51:30 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+let menuVisible = false;
+//Função para ocultar e mostrar o menu
+function mostrarOcultarMenu(){
+    if(menuVisible){
+        document.getElementById("nav").classList ="";
+        menuVisible = false;
+    }else{
+        document.getElementById("nav").classList ="responsive";
+        menuVisible = true;
+    }
+} // Esconder menu quando a opção é selecionada
+function selecionar(){
+    document.getElementById("nav").classList = "";
+    menuVisible = false;
+}
+ // Scroll animation
+const observer = new IntersectionObserver ((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
